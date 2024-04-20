5f0502d9270c74a8e19addf3e34199e0da08280e
MinJungHyun_ <misookhe@gmail.com>
2024-04-20 14:25:51 +0900

    상품 카테고리 목록

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -22,9 +14,9 @@ export class ProductController {
   }
 
   // 2. 상품 조회
-  @Get()
-  findAll() {
-    return this.productService.findAll();
+  @Get('/category/:category')
+  findCategory(@Param('category', ParseIntPipe) category: number) {
+    return this.productService.findCategory(category);
   }
 
   @Get(':id')
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,15 +4,22 @@ import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 type ProductModel = Prisma.ProductDelegate<RejectOptions>;
-const { defaultOptions, getCrud } = new CrudOptions<ProductModel>().setOptions(
-  {},
-);
+const { defaultOptions, getCrud } = new CrudOptions<ProductModel>().setOptions({});
 
 @Injectable()
-export class ProductService extends getCrud<
-  Prisma.ProductGetPayload<typeof defaultOptions>
->() {
+export class ProductService extends getCrud<Prisma.ProductGetPayload<typeof defaultOptions>>() {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma.product, defaultOptions);
+  }
+
+  async findCategory(category: number) {
+    return this.prisma.product.findMany({
+      where: {
+        categoryId: category
+      },
+      include: {
+        images: true
+      }
+    });
   }
 }
