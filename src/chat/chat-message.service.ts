a6265c872bc9f85e2446341c0f30e4169158da02
MinJungHyunA <misookhe@gmail.com>
2024-04-30 19:02:06 +0900

    chat controller 작성

--- src/chat/chat-message.service.ts
+++ src/chat/chat-message.service.ts
@@ -4,14 +4,20 @@ import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 type ChatMessageModel = Prisma.ChatMessageDelegate<RejectOptions>;
-const { defaultOptions, getCrud } =
-  new CrudOptions<ChatMessageModel>().setOptions({});
+const { defaultOptions, getCrud } = new CrudOptions<ChatMessageModel>().setOptions({});
 
 @Injectable()
-export class ChatMessageService extends getCrud<
-  Prisma.ChatMessageGetPayload<typeof defaultOptions>
->() {
+export class ChatMessageService extends getCrud<Prisma.ChatMessageGetPayload<typeof defaultOptions>>() {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma.chatMessage, defaultOptions);
+  }
+
+  async findMessages(chatId: number): Promise<Prisma.ChatMessageGetPayload<typeof defaultOptions>[]> {
+    return this.prisma.chatMessage.findMany({
+      where: {
+        chatId
+      },
+      orderBy: { id: 'asc' }
+    });
   }
 }
--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,42 +1,67 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
-import { UpdateChatDto } from './dto/update-chat.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { User } from 'src/user/user.decorator';
+import { CreateChatMessageDto } from './dto/create-chat-message.dto';
+import { ChatMessageService } from './chat-message.service';
+import { ChatJoinService } from './chat-join.service';
+import { ProductService } from 'src/product/product.service';
 
 @Controller('chat')
 export class ChatController {
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly chatJoinService: ChatJoinService,
+    private readonly chatMessageService: ChatMessageService,
+    private readonly productService: ProductService
+  ) {}
 
-  @Post()
-  create(@Body() createChatDto: CreateChatDto) {
-    return this.chatService.create(createChatDto);
+  @Post(':id')
+  @UseGuards(JwtAuthGuard)
+  async createMessage(@Param('id') id: string, @Body() createChatMessageDto: CreateChatMessageDto, @User() user: any) {
+    const input: CreateChatMessageDto = {
+      chatId: +id,
+      message: createChatMessageDto.message,
+      userId: user.id,
+      type: createChatMessageDto.type
+    };
+    return await this.chatMessageService.create(input);
   }
 
-  @Get()
-  findAll() {
-    return this.chatService.findAll();
+  @Get('/p/:productId')
+  @UseGuards(JwtAuthGuard)
+  async getChatByProductId(@Param('productId', ParseIntPipe) productId: number, @User() user: any) {
+    // 1.있는지 체크
+    let chatId = await this.chatJoinService.findChatByProduct(productId, user.id);
+
+    // 2. 만든적없으면 생성
+    if (chatId == 0) {
+      const chat = await this.chatService.create({ productId });
+      const product = await this.productService.findOne(productId);
+      const ownerId = product?.createdById;
+      await this.chatJoinService.create({ chatId: chat.id, userId: user.id });
+      await this.chatJoinService.create({ chatId: chat.id, userId: ownerId });
+
+      chatId = chat.id;
+    }
+
+    // 3. 메시지반환
+    return await this.chatMessageService.findMessages(chatId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.chatService.findOne(+id);
-  }
+  @Get(':chatId')
+  @UseGuards(JwtAuthGuard)
+  async findOne(@Param('chatId', ParseIntPipe) chatId: number, @User() user: any) {
+    // 해당 userId와 chatId가 있는지 체크, 맞는지 체크
+    const bool = await this.chatJoinService.checkChat(chatId, user.id);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChatDto: UpdateChatDto) {
-    return this.chatService.update(+id, updateChatDto);
-  }
+    // 없으면 에러
+    if (!bool) {
+      throw new Error('Chat not found');
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.chatService.remove(+id);
+    //  메세지 반환
+    return await this.chatMessageService.findMessages(chatId);
   }
 }
